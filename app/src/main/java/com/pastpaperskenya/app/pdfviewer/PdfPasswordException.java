b3c5a2a8a2a4f30f040655eb7b9085d1f7b4b497
Benja <benjambuthia@gmail.com>
2022-09-07 17:42:23 +0300

    add download

---
+++ app/src/main/java/com/pastpaperskenya/app/pdfviewer/PdfPasswordException.java
@@ -0,0 +1,13 @@
+package com.pastpaperskenya.app.pdfviewer;
+
+import java.io.IOException;
+
+public class PdfPasswordException extends IOException {
+    public PdfPasswordException() {
+        super();
+    }
+
+    public PdfPasswordException(String detailMessage) {
+        super(detailMessage);
+    }
+}
