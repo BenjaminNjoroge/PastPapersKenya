f17ae2edc4e148c14080a116ab109571a4af38e6
Benja <benjambuthia@gmail.com>
2023-01-18 07:41:20 +0300

    firebase functions

--- app/src/main/java/com/pastpaperskenya/app/presentation/main/cart/orderfailed/OrderFailedFragment.kt
+++ app/src/main/java/com/pastpaperskenya/app/presentation/main/cart/orderfailed/OrderFailedFragment.kt
@@ -5,14 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.pastpaperskenya.app.databinding.FragmentOrderConfirmedBinding
+import com.pastpaperskenya.app.databinding.FragmentOrderFailedBinding
 
 class OrderFailedFragment : Fragment(){
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    private var _binding: FragmentOrderFailedBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding= FragmentOrderFailedBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.checkDownloadsStatus.setOnClickListener {
+            activity?.finish()
+        }
     }
 }
